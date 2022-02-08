376fd971019004dca86d509688a42a06ae7f86ec
NaZe <dy179912099@163.com>
2022-02-08 20:27:12 +0800

    feat: init my electron app

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  // 如果没有窗口打开则打开一个窗口 (macOS)​
+  //当 Linux 和 Windows 应用在没有窗口打开时退出了，macOS 应用通常即使在没有打开任何窗口的情况下也继续运行，并且在没有窗口可用的情况下激活应用时会打开新的窗口。
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// 关闭所有窗口时退出应用 (Windows & Linux)​
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
